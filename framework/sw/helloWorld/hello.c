09e1070b5208735831afc5fb474a41565a5af4c3
binhkieudo <binhkieudo@gmail.com>
2024-07-11 04:49:09 +0900

    update

--- framework/sw/helloWorld/hello.c
+++ framework/sw/helloWorld/hello.c
@@ -3,35 +3,16 @@
 #include <riscv-pk/encoding.h>
 #include "include/platform.h"
 #include "kprintf.h"
-// #include "kprintf.h"
+
 #define REG32(p, i)	((p)[(i) >> 2])
-
-#define DELAY_TIME 500000
-#define R2L 0
-#define L2R 1
-
-static volatile uint32_t * const gpio = (void *)(GPIO_CTRL_ADDR);
-volatile uint8_t  direction = R2L;
-
-void delay() {
-	for (int i = 0; i < DELAY_TIME; i = i + 1);	
-}
 
 int main(int argc, char **arv) {
   
-  
   REG32(uart, UART_REG_TXCTRL) = UART_TXEN;
-  REG32(gpio, GPIO_OUTPUT_EN) = 0x0000fff0;
 
-  kprintf("Hello world from core %c!!!\r\n", argc + 48);
+  uint32_t mhartid = read_csr(mhartid);
 
-  uint32_t gpio_val = 0x00000010;
+  kprintf("Hello world from core %c!!!\r\n", mhartid + 48);
 
-  while (1) {
-    REG32(gpio, GPIO_OUTPUT_VAL) = gpio_val;
-    gpio_val = gpio_val << 1;
-    if (gpio_val == 0x00010000) gpio_val = 0x00000010;
-    delay();
-  }
   return 0;
 }
